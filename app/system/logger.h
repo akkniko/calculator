5a579d84c85491acc012a8c5e0e100a6f363ac00
Artem <nukonopov@gmail.com>
2025-10-31 11:42:04 +0300

    add utility classes: logger and custom exceptions

---
+++ app/system/logger.h
@@ -0,0 +1,26 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <iostream>
+#include <string>
+
+class Logger
+{
+public:
+    static void info(const std::string &msg)
+    {
+        std::cout << "[INFO] " << msg << "\n";
+    }
+
+    static void warn(const std::string &msg)
+    {
+        std::cout << "[WARN] " << msg << "\n";
+    }
+
+    static void error(const std::string &msg)
+    {
+        std::cerr << "[ERROR] " << msg << "\n";
+    }
+};
+
+#endif // LOGGER_H
